245bcbf1d1522ff83f80ca832e19cf62e0758ae3
LeonardoZan <zanobil@gmail.com>
2017-11-23 00:53:58 +0100

    new file:   03_visibility/main.go

---
+++ 03_visibility/main.go
@@ -0,0 +1,12 @@
+package _3_visibility
+
+//import "fmt"
+//import "package_name"
+
+func main(){
+
+//		fmt.Println(package_name.Variable)			since the first letter of the var is uppercase I can call it
+//		package_name.PrintVar()						from the package "package_name" run function "PrintVar()"
+}
+
+// Declaring a var at a package level scope allow us to use it just by calling its package in the "import" section at the top of the code
