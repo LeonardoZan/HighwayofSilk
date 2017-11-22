3d2a4fab6fe41b1c934ccf466be5f6bab412254c
LeonardoZan <zanobil@gmail.com>
2017-11-22 15:13:35 +0100

    new file:   01_shorthand/01/main.go 	new file:   00_shorthand/02/main.go

---
+++ 01_shorthand/01/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main()  {
+
+	a := 10
+	b := "golang"
+	c := 4.17
+	d := true
+
+	fmt.Printf("%v \n", a)		// by using %v will print the value of the var in a default format
+	fmt.Printf("%v \n", b)
+	fmt.Printf("%v \n", c)
+	fmt.Printf("%v \n", d)
+
+
+}
+
