22515db739843421038739bd779dcba6327d9271
LeonardoZan <zanobil@gmail.com>
2017-12-02 14:32:14 +0100

    renamed:    06_memory_address/02_pointers/main.go -> 06_memory_address/02_pointers/01/main.go 	new file:   06_memory_address/02_pointers/02/main.go

---
+++ 06_memory_address/02_pointers/02/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func zero(z *int){
+
+	fmt.Printf("%p\n",&z)					// address in function zero
+	*z = 0
+
+}
+
+
+func main(){
+
+	x := 5
+	fmt.Printf("%p\n", &x)					// address in main
+
+	zero(&x)
+	fmt.Println(x)									// x is 0
+
+}
